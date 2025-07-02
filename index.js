46244ab4308f6174a346f621b294a05b324b2f40
Ved <vedpawar2254@gmail.com>
2025-07-02 18:54:15 +0530

    v1.0.0

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-export default function ({ types: t }) {
+module.exports = function ({ types: t }) {
   return {
     visitor: {
       UpdateExpression(path) {
         const node = path.node;
         const arg = node.argument;
-        // Match
+
         if (
           t.isMemberExpression(arg) &&
           t.isIdentifier(arg.object, { name: "store" })
         ) {
           const key = arg.property.name;
 
-          // state.count
           const stateProp = t.memberExpression(
             t.identifier("state"),
             t.identifier(key)
           );
 
-          // state.count + 1 or - 1
           const binary = t.binaryExpression(
             node.operator === "++" ? "+" : "-",
             stateProp,
             t.numericLiteral(1)
           );
 
-          // Build: { count: state.count + 1 }
           const objExpr = t.objectExpression([
             t.objectProperty(t.identifier(key), binary),
           ]);
 
-          // Build: state => ({ count: state.count + 1 })
           const arrowFn = t.arrowFunctionExpression(
             [t.identifier("state")],
             objExpr
           );
 
-          // Build: store.set(state => ({ ... }))
           const setCall = t.callExpression(
             t.memberExpression(t.identifier("store"), t.identifier("set")),
             [arrowFn]
@@ -51,25 +46,21 @@ export default function ({ types: t }) {
         const left = node.left;
         const right = node.right;
 
-        // Match
         if (
           t.isMemberExpression(left) &&
           t.isIdentifier(left.object, { name: "store" })
         ) {
           const key = left.property.name;
 
-          // Build: { count: newValue }
           const objExpr = t.objectExpression([
             t.objectProperty(t.identifier(key), right),
           ]);
 
-          // state => ({ count: newValue })
           const arrowFn = t.arrowFunctionExpression(
             [t.identifier("state")],
             objExpr
           );
 
-          //  store.set(state => ({ ... }))
           const setCall = t.callExpression(
             t.memberExpression(t.identifier("store"), t.identifier("set")),
             [arrowFn]
@@ -80,4 +71,4 @@ export default function ({ types: t }) {
       },
     },
   };
-}
+};
